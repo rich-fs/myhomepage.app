c143a2a062bd46eef9a9038d5c5d0b200356a142
Rich Williamson <richard.williamson@thecurve.io>
2023-08-03 21:14:47 +0100

    Login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -12,7 +12,21 @@
 
     <div v-if="showModal" class="login-modal rounded-4 border border-white bg-white-transparent">
       <div class="modal-content text-white">
-        <p>This is a simple modal that appears below the span.</p>
+        <form>
+          <div class="mb-1">
+            <label for="username" class="form-label mb-0">Username</label>
+            <input type="email" class="form-control form-control-sm" id="username">
+          </div>
+          <div class="mb-1">
+            <label for="password" class="form-label mb-0">Password</label>
+            <input type="password" class="form-control form-control-sm" id="password">
+          </div>
+          <div class="mb-1 form-check">
+            <input type="checkbox" class="form-check-input" id="remember">
+            <label class="form-check-label" for="remember">Remember Me</label>
+          </div>
+          <button type="submit" class="btn btn-secondary btn-sm">Submit</button>
+        </form>
       </div>
     </div>
   </div>
